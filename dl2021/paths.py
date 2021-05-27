3951b9c8a7947d2756fa3e174f3ac1b55cba7de7
BorisShirokikh <bg.dustpelt@gmail.com>
2021-05-27 18:22:41 +0300

    FEAT: update baseline paths

--- dl2021/paths.py
+++ dl2021/paths.py
@@ -4,7 +4,6 @@ from .utils import choose_root
 CC359_DATA_PATH = choose_root(
     '/nmnt/x3-hdd/data/da_mri/cc359',
     '/gpfs/data/gpfs0/b.shirokikh/data/cc359',
-    '/',  # TODO: avoiding `FileNotFoundError`
 )
 
 CC359_BASELINE_PATH = choose_root(
@@ -12,8 +11,17 @@ CC359_BASELINE_PATH = choose_root(
     '/',  # TODO: avoiding `FileNotFoundError`
 )
 
+WMH_UNET3D_BASELINE_PATH = choose_root(
+    '/gpfs/data/gpfs0/b.shirokikh/experiments/da/spottune/wmh/baseline/wmh_unet3d_one2all/',
+    '/nmnt/x4-hdd/experiments/da_exps/wmh/baseline/wmh_unet3d_one2all/',
+)
+
+WMH_DM_BASELINE_PATH = choose_root(
+    '/gpfs/data/gpfs0/b.shirokikh/experiments/da/spottune/wmh/baseline/wmh_dm_one2all/',
+    '/nmnt/x4-hdd/experiments/da_exps/wmh/baseline/wmh_dm_one2all/',
+)
+
 WMH_DATA_PATH = choose_root(
     '/nmnt/x3-hdd/data/da_mri/wmh_ants',
     '/gpfs/data/gpfs0/b.shirokikh/data/wmh_ants',
-    '/nmnt/media/home/anastasia_kurmukova/wmh_ants',
 )
